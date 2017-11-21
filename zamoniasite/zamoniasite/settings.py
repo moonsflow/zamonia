eb5c8e46237384850b8997eecef38b05a5bf6a3d
Daniel <bomipapa@gmail.com>
2017-11-22 08:21:18 +0900

    update setting

--- zamoniasite/zamoniasite/settings.py
+++ zamoniasite/zamoniasite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')l_$b+%_lc@j=0o5sxyv$%agj)d#)(^fy9+asbcuk_ic1#*m2+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'booknote',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
